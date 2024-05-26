8f5a94263cb38471a5a5ad34fc4669ed3ff39dd8
shwetangi03 <113777115+shwetangi03@users.noreply.github.com>
2024-05-26 11:21:10 +0530

    Prerendering the powers of Next JS

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -1,5 +1,4 @@
 import MeetupDetail from "@/components/meetups/MeetupDetail";
-import React, { Fragment } from "react";
 
 const MeetupDetails = () => {
   return (
@@ -12,5 +11,45 @@ const MeetupDetails = () => {
     />
   );
 };
+
+export async function getStaticPaths(context) {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+      {
+        params: {
+          meetupId: "m3",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://images.unsplash.com/photo-1502989642968-94fbdc9eace4?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixid=MnwxfDB8MXxyYW5kb218MHx8bmF0dXJlLFRlY2hub2xvZ3l8fHx8fHwxNzE2NjQ0MDg1&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=400",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some street 8 Some City",
+        description: "The meetup description",
+      },
+    },
+  };
+}
 
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,52 @@
 import MeetupList from "@/components/meetups/MeetupList";
-import React from "react";
 
-const HomePage = () => {
-  const DUMMY_MEETUPS = [
-    {
-      id: "m1",
-      title: "A first meetup",
-      image:
-        "https://images.pexels.com/photos/593655/pexels-photo-593655.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      address: "sme address 54686 some city",
-      description: "This is a first meetup",
-    },
-    {
-      id: "m2",
-      title: "A first meetup",
-      image:
-        "https://images.pexels.com/photos/355465/pexels-photo-355465.jpeg?auto=compress&cs=tinysrgb&w=600",
-      address: "sme address 54686 some city",
-      description: "This is a first meetup",
-    },
-    {
-      id: "m3",
-      title: "A first meetup",
-      image:
-        "https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=600",
-      address: "sme address 54686 some city",
-      description: "This is a first meetup",
-    },
-  ];
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A first meetup",
+    image:
+      "https://images.pexels.com/photos/593655/pexels-photo-593655.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    address: "sme address 54686 some city",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A first meetup",
+    image:
+      "https://images.pexels.com/photos/355465/pexels-photo-355465.jpeg?auto=compress&cs=tinysrgb&w=600",
+    address: "sme address 54686 some city",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m3",
+    title: "A first meetup",
+    image:
+      "https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=600",
+    address: "sme address 54686 some city",
+    description: "This is a first meetup",
+  },
+];
+const HomePage = (props) => {
+  return <MeetupList meetups={props.meetups} />;
 };
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 1,
+  };
+}
 
 export default HomePage;
